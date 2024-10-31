79b176045d30925fb7e191b2bed3085537cce7ea
kaabrealston <thecalpz@gmail.com>
2024-10-31 12:47:02 +0000

    learning python

---
+++ unit3_python/pythonFoundation.py
@@ -0,0 +1,34 @@
+#data types = are the fundimental building
+
+
+# intergers = whole numbers
+# strings = any charecters wrapped in quotes
+# boolean = true and false
+# we outputdata using the print function
+print(200 * 8)
+print ('welcome to python. !@#')
+Print (True)
+
+() #round brackets = function
+{} #curly brackets = objects
+[] # square brackets = lists
+
+# varubles = is a contanier for data
+# we write varibles by first declaring/ giving it a name
+# we use the equal sign
+# we then give it data
+name = 'write some text'
+meassage = 'goodmorning'
+
+# conatenation = combining strings
+print (message, name)
+
+story = "i will be going to"
+ location =''
+ time =''
+ print(story)
+
+valA=10
+valB=20
+
+print( valA,valB)
