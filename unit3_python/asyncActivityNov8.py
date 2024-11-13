f5691dac8af6396e6f9c71cafa16d8f032598317
kaabrealston <thecalpz@gmail.com>
2024-11-13 13:46:49 +0000

    learning more python

---
+++ unit3_python/asyncActivityNov8.py
@@ -0,0 +1,54 @@
+## Async Activity November 8th, 2024 ##
+ #inside of your unit 3 folder create a new file called 
+
+#asyncActivityNov8.py OR asyncActivityNov8.js, depending on which programming language you are learning. 
+#Please follow the directions provided and answer the following questions.This will be do at the end of class. This will need to be submitted whether you are present or not. 
+#Good luck.
+#1. What does it "string concatenation" mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal. 
+
+
+#- if you are learning JavaScript you will be using the 
+#console.log() function.
+#- if you are learning Pyhon you will be using the print() function.
+#2. Please list and describe three rules for creating variable names. Please provide your answer as a string.
+
+'1.  you have a number or symbol start a varuble name.'
+
+'2. Varible should ideally be short and descriptive.'
+
+'3. we can format our varinbles names using camel case, snake case and  '
+#3. Define each of the following variable name formats then provide a code example of how you would write them.
+#- Camel case
+#- Snake case
+#- Pascal case
+#4. Find the missing variable for each of the following assignment operaor code snippets
+#***Python Version***
+
+
+x=10    #10
+
+y=2   #2
+
+z= y*2 #4
+
+printx > 1  z==4)
+(
+
+#***JavaScript Version***
+#let a = '?'
+#let b = a *b
+#print(b) // the output in the terminal should be 700. 
+#5. Use the logical operators and evaluate the following variables
+#to get the requested result.
+#***Python Version***
+#- which logical operator would you use to reflect that the result between the code expressions are **true**. use the print function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+#x= 10
+#y= 2
+#z= y*2
+#x > 1 _____ z==4 
+#***JavaScript Version***
+#- which logical operator would you use to reflect that the result between the code expressions below are **false**. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+#var x= 12
+#var y= 2
+#var z= 12/4
+#x <= 12 _____ z < 10
---
+++ unit3_python/operatorReview.py
@@ -0,0 +1,30 @@
+# arithmetic operators - math processes. addition subtraction multiplication,
+deposit= 100.0
+withdrawamount= 0.0
+
+checkingAccountBalance = 0.0
+
+savingsAccountBalce = 300.0
+
+investmentAccount = 0.0
+
+print( depostiAmount + savingsAccountBalce)
+
+# tracker runner app arithmetic operator examples
+firstRunTime= 20.00
+secondRuntime = 18.00
+
+runningDifference = firstRunTime-secondRuntime
+
+
+# logical operators
+
+# comparison operators
+
+# assignment operators- doiing arithmetic after tthe assignment of value
+
+
+jeans = 200.00
+jeans *=0.25
+
+checkingAmount=''
